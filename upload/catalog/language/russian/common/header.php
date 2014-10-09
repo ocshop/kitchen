654bdb98345c39f22142b0cc5973d7bfa2f7d9a8
ocshop <info@vilkov.info>
2014-10-10 01:02:52 +0300

    fix авторизация

--- upload/catalog/language/russian/common/header.php
+++ upload/catalog/language/russian/common/header.php
@@ -1,16 +1,31 @@
 <?php
 // Text
-$_['text_home']     = 'Главная';
-$_['text_category'] = 'Меню';
-$_['text_wishlist'] = 'Закладки (%s)';
-$_['text_shopping_cart']  = 'Корзина';
-$_['text_search']   = 'Поиск';
-$_['text_welcome']  = '<li><a href="%s"><i class="icon-lock"></i> Вход</a></li>';
-$_['text_logged']   = '<li>Вы вошли как <a href="%s">%s</a> <b>(</b> <a href="%s">Выйти</a> <b>)</b></li>';
-$_['text_account']  = 'Кабинет';
-$_['text_checkout'] = 'Оформление заказа';
-$_['text_blog']	    = 'Блог';
-$_['text_special']  = 'Акции';
-$_['text_latest']   = 'Новинки';
-$_['text_brands']   = 'Производители';
+$_['text_home']     		= 'Главная';
+$_['text_wishlist'] 		= 'Закладки (%s)';
+$_['text_shopping_cart']  	= 'Корзина';
+$_['text_search']   		= 'Поиск';
+$_['text_account']  		= 'Кабинет';
+$_['text_checkout'] 		= 'Оформление заказа';
+$_['text_blog']	    		= 'Блог';
+$_['text_special']  		= 'Акции';
+$_['text_latest']   		= 'Новинки';
+$_['text_brands']   		= 'Производители';
+$_['text_welcome']       	= '<li class="login_h"><a href="%s"><i class="icon-lock"></i>Войти</a></li> <li><a href="%s"><i class="icon-file-alt"></i>Зарегистрироваться</a></li>  ';
+$_['text_logged']       	= '<li class="login_h"><a href="%s"><i class="icon-file-alt"></i>%s</a></li>  <li><a href="%s"><i class="icon-lock"></i>Выйти</a></li> ';
+$_['text_language']     	= 'Язык';
+$_['text_currency']     	= 'Валюта';
+$_['text_information']  	= 'Информация';
+$_['text_service']      	= 'Служба поддержки';
+$_['text_extra']        	= 'Дополнительно';
+$_['text_contact']      	= 'Связаться с нами';
+$_['text_return']       	= 'Возвраты';
+$_['text_sitemap']      	= 'Карта сайта';
+$_['text_manufacturer'] 	= 'Производители';
+$_['text_voucher']      	= 'Подарочные сертификаты';
+$_['text_affiliate']    	= 'Партнёрская программа';
+$_['text_account']      	= 'Личный кабинет';
+$_['text_order']        	= 'История заказов';
+$_['text_newsletter']   	= 'Рассылка';
+$_['text_category']     	= 'Категории';
+$_['text_latest_added']   	= 'В вашей корзине:';
 ?>
