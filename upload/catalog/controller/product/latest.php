cd3477f3f87474e4d44f16ccb541aca931004b01
ocshop <info@vilkov.info>
2014-10-10 19:27:33 +0300

    fix latest noindex

--- upload/catalog/controller/product/latest.php
+++ upload/catalog/controller/product/latest.php
@@ -15,6 +15,7 @@ class ControllerProductLatest extends Controller {
 		
 		if (isset($this->request->get['sort'])) {
 			$sort = $this->request->get['sort'];
+			$this->document->setRobots('noindex,follow');
 		} else {
 			$sort = 'pd.date';
 		}
@@ -27,12 +28,14 @@ class ControllerProductLatest extends Controller {
 			 
   		if (isset($this->request->get['page'])) {
 			$page = $this->request->get['page'];
+			$this->document->setRobots('noindex,follow');
 		} else {
 			$page = 1;
 		}
 		
 		if (isset($this->request->get['limit'])) {
 			$this->data['limit'] = $this->request->get['limit'];
+			$this->document->setRobots('noindex,follow');
 		} else {
 			$this->data['limit'] = $this->config->get('config_catalog_limit');
 		}
